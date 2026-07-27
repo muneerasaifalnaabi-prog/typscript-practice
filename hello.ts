2b475a1f2311d610c280cb268bcdced313843ba8
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-07-27 10:01:20 +0400

    TypeScript practice

--- hello.ts
+++ hello.ts
@@ -9,13 +9,19 @@
 //     {name:"muneera",scores:98}
 // ]
 
-function describe(name:string,score:number,cohort:string ="Code Champs",note?:string):string{
-    return  `${name} scored ${score} ${cohort} ${note??""}`;
-}
+// function describe(name:string,score:number,cohort:string ="Code Champs",note?:string):string{
+//     return  `${name} scored ${score} ${cohort} ${note??""}`;
+// }
 
 
-console.log(describe("Ishaq",99));
-console.log(describe("Ishaq",99,"Code Champs","late Submit"));
+// console.log(describe("Ishaq",99));
+// console.log(describe("Ishaq",99,"Code Champs","late Submit"));
 
 
 // export {};
+
+//any data type for dynamic data type 
+let s:any =34;
+s="name";
+s.method1();
+s.b.c.d;
